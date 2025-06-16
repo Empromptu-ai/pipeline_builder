c69be27538be65bffdf6f45648652d46b039ef30
Sean Robinson <greatblondelf@gmail.com>
2025-06-16 16:39:19 -0700

    space tweak

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -38,7 +38,7 @@ export function Header() {
         <div className="i-ph:sidebar-simple-duotone text-xl" />
         <a href="/" className="text-2xl font-semibold text-accent flex items-center">
           {/* <span className="i-bolt:logo-text?mask w-[46px] inline-block" /> */}
-          Empromptu
+          Empromptu     
         </a>
       </div>
       <LogoutButton />
