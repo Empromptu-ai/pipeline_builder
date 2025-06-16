02e302797c8c14684dd3ab6ddb02e40567804a57
Sean Robinson <greatblondelf@gmail.com>
2025-06-16 16:50:30 -0700

    formatting

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -34,17 +34,19 @@ export function Header() {
         },
       )}
     >
-      <div className="flex items-center gap-2 text-bolt-elements-textPrimary cursor-pointer">
-        <div className="i-ph:sidebar-simple-duotone text-xl" />
-        <a href="/" className="text-2xl font-semibold text-accent flex items-center">
-          {/* <span className="i-bolt:logo-text?mask w-[46px] inline-block" /> */}
-          Empromptu     
-        </a>
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2 text-bolt-elements-textPrimary cursor-pointer">
+          <div className="i-ph:sidebar-simple-duotone text-xl" />
+          <a href="/" className="text-2xl font-semibold text-accent flex items-center">
+            {/* <span className="i-bolt:logo-text?mask w-[46px] inline-block" /> */}
+            Empromptu 
+          </a>
+        </div>
+        <LogoutButton />
+        <span className="flex-1 px-4 truncate text-center text-bolt-elements-textPrimary">
+          <ClientOnly>{() => <ChatDescription />}</ClientOnly>
+        </span>
       </div>
-      <LogoutButton />
-      <span className="flex-1 px-4 truncate text-center text-bolt-elements-textPrimary">
-        <ClientOnly>{() => <ChatDescription />}</ClientOnly>
-      </span>
       {chat.started && (
         <ClientOnly>
           {() => (
