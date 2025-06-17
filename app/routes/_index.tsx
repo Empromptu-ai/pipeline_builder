f8b69f63aa1f912d25507d971dce8a02687e53c8
Sean Robinson <greatblondelf@gmail.com>
2025-06-16 18:25:54 -0700

    move to https endpoint

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -24,7 +24,7 @@ function OptimizerView() {
   return (
     <div className="flex-1 relative">
       <iframe
-        src="http://analytics.empromptu.ai:8080/"
+        src="https://analytics.impromptu-labs.com/"
         className="w-full h-full border-0"
         title="Optimizer App"
         sandbox="allow-scripts allow-same-origin allow-forms allow-popups allow-top-navigation"
