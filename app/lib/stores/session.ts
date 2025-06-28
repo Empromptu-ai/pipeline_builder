fee843e043c682df821be532926efed18f03e80e
Sean Robinson <sean@madronalabs.co>
2025-06-28 01:17:52 -0700

    removing crypto package because they recompiled it and broke it on the client side awhile back.  Apparently on purpose.

--- app/lib/stores/session.ts
+++ app/lib/stores/session.ts
@@ -1,7 +1,18 @@
 import { atom } from 'nanostores';
-import { randomBytes } from 'crypto';
+// import { randomBytes } from 'crypto';
 
 // a session ID meant to be random for each user+session
+// export const sessionUid = atom<string | undefined>(undefined);
+// const description = randomBytes(8).toString('hex');
+// sessionUid.set(description);
+
+
+import { atom } from 'nanostores';
+
+function generateUID() {
+  return Math.random().toString(36).substring(2) + Date.now().toString(36);
+}
+
 export const sessionUid = atom<string | undefined>(undefined);
-const description = randomBytes(8).toString('hex');
+const description = generateUID();
 sessionUid.set(description);
