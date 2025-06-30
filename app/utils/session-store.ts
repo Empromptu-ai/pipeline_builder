60aa4519a76beb3869285cd288fab2fd3f6d77a4
Sean Robinson <greatblondelf@gmail.com>
2025-06-29 19:47:39 -0700

    trying globals

--- app/utils/session-store.ts
+++ app/utils/session-store.ts
@@ -9,12 +9,25 @@
 // }
 
 
-let currentSessionUid: string | null = null;
+// let currentSessionUid: string | null = null;
 
-export function setSessionUid(uid: string): void {
-  currentSessionUid = uid;
+// export function setSessionUid(uid: string): void {
+//   currentSessionUid = uid;
+// }
+
+// export function getSessionUid(): string | null {
+//   return currentSessionUid;
+// }
+
+
+declare global {
+  var currentSessionUid: string | undefined;
 }
 
-export function getSessionUid(): string | null {
-  return currentSessionUid;
+export function setSessionUid(uid: string): void {
+  global.currentSessionUid = uid;
+}
+
+export function getSessionUid(): string | undefined {
+  return global.currentSessionUid;
 }
